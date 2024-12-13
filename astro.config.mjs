d9bf97d63d14ec4d139a16de7058b1d39b6b335b
Rensoconese <rensoconese@gmail.com>
2024-12-12 22:24:13 -0300

    3product update

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,42 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), mdx()]
+  site: 'https://evos.com.ar',
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }), 
+    mdx()
+  ],
+  output: 'static',
+  build: {
+    inlineStylesheets: 'auto',
+  },
+  vite: {
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        output: {
+          sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
+            return relativeSourcePath;
+          }
+        }
+      }
+    },
+    optimizeDeps: {
+      exclude: []
+    },
+    server: {
+      watch: {
+        ignored: ['**/node_modules/**', '**/.git/**']
+      }
+    }
+  },
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  }
 });
